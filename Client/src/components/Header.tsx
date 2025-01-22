a7afd6f2fa4974aa53f502a0c6ddc50f408ad966
dev-code-create <ayushkankale04@gmail.com>
2025-01-22 17:40:08 +0530

    Created Layout and Header Component

---
+++ Client/src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img
+            src={isDark ? "/logo.png" : "/logo2.png"}
+            alt="website logo"
+            className="h-14 "
+          />
+        </Link>
+        <div>
+          {/* Search */}
+          {/* theme toggle */}
+          <div
+            onClick={() => {
+              setTheme(isDark ? "light" : "dark");
+            }}
+            className={`flex items-center cursor-pointer transition-transform duration-500 ${
+              isDark ? "rotate-180" : "rotate-0"
+            }`}
+          >
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all hover:rotate-45" />
+            ) : (
+              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all hover:rotate-12" />
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
