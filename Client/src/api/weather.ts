a21098e6f1daa08816af9cae2187de2506402896
dev-code-create <ayushkankale04@gmail.com>
2025-01-22 23:37:34 +0530

    FetchData url logic

--- Client/src/api/weather.ts
+++ Client/src/api/weather.ts
@@ -1,16 +1,23 @@
-import { API_CONFIG } from "./config"
+import { API_CONFIG } from "./config";
 
 class WeatheAPI {
-  private createURL(endpoint) {
-    private createUrl(endpoint: string,params:Record<string,string | number>){
-      const searchParam = new URLSearchParams({
-        appid: API_CONFIG.API_KEY,
-        ...params,
-      })
-      return `${endpoint}?${searchParam.toString()}`
+  private createUrl(endpoint: string, params: Record<string, string | number>) {
+    const searchParam = new URLSearchParams({
+      appid: API_CONFIG.API_KEY,
+      ...params,
+    });
+    return `${endpoint}?${searchParam.toString()}`;
+  }
+
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Weather API Error: ${response.statusText}`);
     }
-  
-  private fetchData() {}
+
+    return response.json();
+  }
   async getCurrentWeather() {}
   async getForecast() {}
   async reverseGeoCode() {}
