e6f14d28d40f8f4a55213db0055828fd17c059c3
dev-code-create <ayushkankale04@gmail.com>
2025-01-23 14:18:17 +0530

    Created useGeolocation hook

--- Client/src/pages/weather-dashboard.tsx
+++ Client/src/pages/weather-dashboard.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { RefreshCcw } from "lucide-react";
+
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  const { coordinates, error, getLocation, isLoading } = useGeolocation();
+  console.log(coordinates);
+  return (
+    <div className="space-y-4 ">
+      {/* Favourite Cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-bold tracking-tight ">My Location</h1>
+        <Button variant={"outline"} size={"icon"}>
+          <RefreshCcw className="h-4 w-4" />
+        </Button>
+      </div>
+      {/* Current and Hourly weather */}
+    </div>
+  );
 };
 
 export default WeatherDashboard;
