0ddeafb1d75a2c2df4e0072939bbe5d7102cb4ba
dev-code-create <ayushkankale04@gmail.com>
2025-01-25 00:01:06 +0530

    hourly temperature done

--- Client/src/components/hourly-temperature.tsx
+++ Client/src/components/hourly-temperature.tsx
@@ -53,13 +53,17 @@ const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
                             <span className="text-[0.75rem] uppercase text-muted-foreground">
                               Temperature
                             </span>
-                            <span>{payload[0].value}°</span>
+                            <span className="font-bold">
+                              {payload[0].value}°
+                            </span>
                           </div>
                           <div className="flex flex-col">
                             <span className="text-[0.70rem] uppercase text-muted-foreground">
                               Feels Like
                             </span>
-                            <span>{payload[1].value}°</span>
+                            <span className="font-bold">
+                              {payload[1].value}°
+                            </span>
                           </div>
                         </div>
                       </div>
