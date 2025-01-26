15074c490436b7edd6eceedf88a1837a7d5bde53
dev-code-create <ayushkankale04@gmail.com>
2025-01-26 18:26:07 +0530

    SearchHistory hook confirgured

--- Client/src/components/city-search.tsx
+++ Client/src/components/city-search.tsx
@@ -9,18 +9,29 @@ import {
   CommandList,
   CommandSeparator,
 } from "./ui/command";
-import { Loader2, Search } from "lucide-react";
+import { Clock, Loader2, Search, XCircle } from "lucide-react";
 import { uselocationSearch } from "@/hooks/use-weather";
 import { useNavigate } from "react-router-dom";
+import { useSearchHistory } from "@/hooks/use-search-history";
 
 const CitySearch = () => {
   const [open, setOpen] = useState(false);
   const [query, setQuery] = useState("");
   const { data: locations, isLoading } = uselocationSearch(query);
   const navigate = useNavigate();
+  const { history, clearHistory, addToHistory } = useSearchHistory();
 
   const handleSelect = (cityData: string) => {
     const [lat, lon, name, country] = cityData.split("|");
+
+    addToHistory.mutate({
+      query,
+      name,
+      lat: parseFloat(lat),
+      lon: parseFloat(lon),
+      country,
+    });
+
     setOpen(false);
     navigate(`/city/${name}??lat=${lat}&lon=${lon}`);
   };
@@ -50,13 +61,50 @@ const CitySearch = () => {
           </CommandGroup>
           <CommandSeparator />
 
-          <CommandGroup heading="Recent Searches">
-            <CommandItem>Calendar</CommandItem>
-          </CommandGroup>
-          <CommandSeparator />
+          {history.length > 0 && (
+            <>
+              <CommandGroup heading="Recent Searches">
+                <div>
+                  <p>Recent Searches</p>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    onClick={() => {
+                      clearHistory.mutate();
+                    }}
+                  >
+                    <XCircle className="h-4 w-4" />
+                    Clear
+                  </Button>
+                </div>
+                {history.map((location) => {
+                  return (
+                    <CommandItem
+                      key={`${location.lat}-${location.lon}`}
+                      value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                      onSelect={handleSelect}
+                    >
+                      <Clock className="mr-2 h-4 w-4 text-muted-foreground" />
+                      <span>{location.name}</span>
+                      {location.state && (
+                        <span className="text-sm text-muted-foreground ">
+                          , {location.state}
+                        </span>
+                      )}
+                      <span className="text-sm text-muted-foreground">
+                        , {location.country}
+                      </span>
+                    </CommandItem>
+                  );
+                })}
+                <CommandItem>Calendar</CommandItem>
+              </CommandGroup>
+              <CommandSeparator />
+            </>
+          )}
 
           {locations && locations.length > 0 && (
-            <CommandGroup heading="Favourites">
+            <CommandGroup heading="Suggestions">
               {isLoading && (
                 <div className="flex items-center justify-center p-4">
                   <Loader2 className="h-4 w-4 animation-spin" />
